99939373e18f053730aa97159fe1d3dd56f6f515
MMDVB <mohammad98mahdi@gmail.com>
2023-01-09 20:52:33 +0330

    Test2

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
-  public static void main (string[] args) {
+  public static void main (String[] args) {
     System.out.println("Hi To Java World");
     System.out.println("I'm learning.");
     System.out.println("Can you Help me ??!");
