20432c1c56bc6d4d53040a0ce9bf3894d6a2b8ed
MMDVB <mohammad98mahdi@gmail.com>
2023-01-09 20:14:35 +0330

    Test2

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
   public static void main (string[] args) {
-    System.out.println("Hello World!");
+    System.out.println("سلام به دنیای جاوا!");
+    System.out.println("من در حال یاد گیری جاوا هستم.");
+    System.out.println("Can you Help me ??!")
   }
 }
