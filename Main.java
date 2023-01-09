99af0bc076cd11610c35061c5ab46817350df938
MMDVB <mohammad98mahdi@gmail.com>
2023-01-09 20:58:21 +0330

    Test2

--- Main.java
+++ Main.java
@@ -4,4 +4,5 @@ public class Main {
     System.out.println("I'm learning.");
     System.out.println("Can you Help me ??!");
   }
-}
+} /* Ta inja kami ba barname haye IJ va gitbash ashna shodam va fahmidam chjori
+     Ba String ham ashna shodam ke matni manand Hello ra zakhire mikone  */
