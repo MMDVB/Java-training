19c1865c6c8c6f46254a694f730a2919246a3660
MMDVB <120246758+MMDVB@users.noreply.github.com>
2023-01-09 20:38:54 +0330

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main {
   public static void main (string[] args) {
     System.out.println("سلام به دنیای جاوا!");
     System.out.println("من در حال یاد گیری جاوا هستم.");
-    System.out.println("Can you Help me ??!")
+    System.out.println("Can you Help me ??!");
   }
 }
