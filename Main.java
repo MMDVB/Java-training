e7ce6140e475af3ad823948e0e45f96539f74a7e
MohammadMahdi <Mohammad98mahdi@gmail.com>
2023-01-31 21:09:51 +0330

    Test3

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
 public class Main {
-  public static void main (String[] args) {
+  public static void main(String[] args) {
     System.out.println("Hi To Java World");
     System.out.println("I'm learning.");
     System.out.println("Can you Help me ??!");
   }
-} /* Ta inja kami ba barname haye IJ va gitbash ashna shodam va fahmidam chjori
+}
+/* Ta inja kami ba barname haye IJ va gitbash ashna shodam va fahmidam chjori
      Ba String ham ashna shodam ke matni manand Hello ra zakhire mikone  */
---
+++ Test.java
@@ -0,0 +1,11 @@
+public class Test {
+    public static void main (String[] args) {
+        int x = 80;
+    System.out.println(x);
+
+        int p = 4444 + 2323;
+        System.out.println(p);
+
+
+ }
+}
