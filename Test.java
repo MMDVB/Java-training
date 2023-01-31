47823bedd4dfac040b62ac66950ac48cf41fad8d
MohammadMahdi <Mohammad98mahdi@gmail.com>
2023-01-31 21:50:20 +0330

    Test4

--- Test.java
+++ Test.java
@@ -2,10 +2,26 @@ public class Test {
     public static void main (String[] args) {
         int x = 80;
     System.out.println(x);
+ //Jam sade
+        String myText = "Mathtime Easy";
+        int s = 4444 + 2323;
+        System.out.println(myText);
+        System.out.println(s);
 
-        int p = 4444 + 2323;
-        System.out.println(p);
-
-
+//jam pichide
+        String myText2 = "Mathtime Hard";
+        char LVL1 = 'A';
+        int sum1 = 145 + 200;
+        char LVL2 = 'B';
+        int sum2 = sum1 + 500;
+        char LVL3 = 'C';
+        int sum3 = sum2 + sum1;
+        System.out.println(myText2);
+        System.out.println(LVL1);
+        System.out.println(sum1);
+        System.out.println(LVL2);
+        System.out.println(sum2);
+        System.out.println(LVL3);
+        System.out.println(sum3);
  }
 }
