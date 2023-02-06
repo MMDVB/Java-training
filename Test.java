5a0edaa7c1c28c593b84c6e94c7e2fc48e0b0e8f
MohammadMahdi <Mohammad98mahdi@gmail.com>
2023-02-06 23:40:32 +0330

    Test6

--- Test.java
+++ Test.java
@@ -26,23 +26,57 @@ public class Test {
         char LVL3 = 'C';
         int sum3 = sum2 + sum1;
         System.out.println(myText2);
-        System.out.println(LVL1);
-        System.out.println(sum1);
-        System.out.println(LVL2);
-        System.out.println(sum2);
-        System.out.println(LVL3);
-        System.out.println(sum3);
+         System.out.println(LVL1);
+           System.out.println(sum1);
+            System.out.println(LVL2);
+             System.out.println(sum2);
+              System.out.println(LVL3);
+                System.out.println(sum3);
 
         int myInt = 9;
         double  myDouble = myInt;
                 System.out.println(myInt);
-                System.out.println(myDouble);
+                    System.out.println(myDouble);
 
         double myDouble2 = 9.16d;
         int myInt2 = (int) myDouble2;
-        System.out.println(myDouble2);
-        System.out.println(myInt2);
+            System.out.println(myDouble2);
+                System.out.println(myInt2);
+
+        // Test java switch
+
+        int day = 5;
+         switch (day) {
+             case 1:
+                 System.out.println("Shanbe");
+                 break;
+             case 2:
+                 System.out.println("Yekshanbe");
+                 break;
+             case 3:
+                 System.out.println("Doshanbe");
+                 break;
+             default:
+                 System.out.println("Dari Eshtebah mizani");
+         }
+
+         //test if..else
+        int xo = 60;
+        int yo = 20;
+        if (xo > yo) {
+            System.out.println("Halet Chetore?");
+        } else if (xo < 80) {
+            System.out.println("Gomsho");
+        } else {
+            System.out.println("Bye");
 
 
+        }
+
+        int j = 20;
+          while ( j < 60) {
+              System.out.println(j);
+              j++;
+          }
  }
 }
