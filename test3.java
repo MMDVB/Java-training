3854621c26f67ac0b856d0e0a1bcc69129526371
MohammadMahdi <Mohammad98mahdi@gmail.com>
2023-02-10 00:35:59 +0330

    Test6

---
+++ test3.java
@@ -0,0 +1,11 @@
+public class test3 {
+    static void myMethod(String fname) {
+        System.out.println(fname + " Rajabzadeh");
+    }
+        public static void main(String [] args) {
+            myMethod("Alireza");
+            myMethod("Saeid");
+            myMethod("Samira");
+            myMethod("MohammadMahdi");
+     }
+}
